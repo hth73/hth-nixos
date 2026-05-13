4beb9c5592df239fcb24f559564bdc4d40148a4f
Helmut Thurnhofer <helmut.thurnhofer@live.de>
2026-05-13 16:14:41 +0200

    last changes in hth.nix configuration

--- home-manager/hth.nix
+++ home-manager/hth.nix
@@ -9,14 +9,14 @@
   home.file."wallpaper".source = ../dotfiles/hth/wallpaper;
   home.file.".bash_profile".source = ../dotfiles/common/.bash_profile;
   home.file.".bashrc".source = ../dotfiles/hth/.bashrc;
-  home.file.".".gitconfigsource = ../dotfiles/hth/.gitconfig;
+  home.file.".gitconfig".source = ../dotfiles/hth/.gitconfig;
   home.file.".LESS_TERMCAP".source = ../dotfiles/hth/.LESS_TERMCAP;
 
   # Config Files User Environment
   home.file.".config/bat".source = ../dotfiles/config/bat;
   home.file.".config/btop".source = ../dotfiles/config/btop;
-  home.file.".config/pcmanfm".source = .../dotfiles/config/pcmanfm;
-  home.file.".config/qtile".source = .../dotfiles/config/qtile;
+  home.file.".config/pcmanfm".source = ../dotfiles/config/pcmanfm;
+  home.file.".config/qtile".source = ../dotfiles/config/qtile;
 
   programs.alacritty = {
     enable = true;
