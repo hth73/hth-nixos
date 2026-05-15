1bf18b3b84e3d1ed7c0ca9268cc406572b2e7713
Helmut Thurnhofer <hellemon@live.de>
2026-05-15 13:37:13 +0200

    Add disko input to outputs

--- flake.nix
+++ flake.nix
@@ -5,13 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, disko, ... }: {
     nixosConfigurations.mynixos =
       nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          disko.nixosModules.disko
           ./configuration.nix
           home-manager.nixosModules.home-manager
         ];
